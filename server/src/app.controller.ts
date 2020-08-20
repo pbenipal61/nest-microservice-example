0314a7d4dfe15857c253984c9d29c0f575c3eaef
Singh, Prabhjot <t8sipr00@students.oamk.fi>
2020-08-20 09:36:19 +0300

    initialised microservice service

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { EventPattern } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  
+  constructor() { }
+  @EventPattern('message_pattern')
+  async handleMessagePrinted(data: Record<string, unknown>) {
+    console.log(data.text);
   }
 }
